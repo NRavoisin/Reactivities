3b20f65ac0b987cbf678b8ed7a1d337f857a02a7
Nathan Ravoisin <nravoisin@intermade.lu>
2022-03-14 19:03:42 +0200

    End of section 7.

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-interface Props {
-    openForm: () => void;
-}
+export default function NavBar() {
+    const {activityStore} = useStore();
 
-export default function NavBar({openForm} : Props)
-{
-    return(
+    return (
         <Menu inverted fixed = 'top'>
             <Container>
                 <Menu.Item header>
@@ -16,7 +14,7 @@ export default function NavBar({openForm} : Props)
                 </Menu.Item>
                 <Menu.Item name = 'Activities'/>
                 <Menu.Item>
-                    <Button onClick = {openForm} positive content = 'Create activity'/>
+                    <Button onClick = {() => activityStore.openForm()} positive content = 'Create activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
